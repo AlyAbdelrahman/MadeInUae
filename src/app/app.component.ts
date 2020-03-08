cb75e99de7fb2512da0c1fc32d01c98f7e4b2edb
Aly Abdelrahman <aly.m.abdelrahman@outlook.com>
2020-03-08 20:48:48 +0200

    change style and language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { DomSanitizer, SafeStyle, SafeResourceUrl } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,8 @@ export class AppComponent implements OnInit  {
   cssUrl: SafeResourceUrl ;
   lang:string='ar';
   currentL:string;
-  
-  constructor(public sanitizer: DomSanitizer, public translate: TranslateService) {
+  files = ["assets/css/style-rtl.css","assets/css/style-ltr.css"]
+  constructor(public sanitizer: DomSanitizer, public translate: TranslateService  ) {
     translate.addLangs(['en', 'ar']);
     translate.setDefaultLang(this.lang);
   }
@@ -24,12 +25,23 @@ export class AppComponent implements OnInit  {
 
   switchLang() {
     
+    var oldlink = document.getElementById("mycss"); // by default rtl
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("id","mycss");
       if(this.lang=='en'){
         this.translate.use('ar');
+        newlink.setAttribute("href", this.files[0]); 
+        document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+    
         this.lang='ar'
       }
       else{
         this.translate.use('en');
+        newlink.setAttribute("href", this.files[1]); 
+        document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+    
         this.lang='en'
       }
 
